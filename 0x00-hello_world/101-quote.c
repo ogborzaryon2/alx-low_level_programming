9593226f32ca0fee5d8547d83f1367602f56bc37
zaryonogbor <ogborfrancisco@gmail.com>
2022-04-07 14:30:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return:1
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
 return (1);
 }
